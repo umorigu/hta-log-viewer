8899932cdb08e7d70b595ce058fe7f0e0ffc4455
Moriguchi, Hirokazu <umorigu@gmail.com>
2011-01-09 01:03:26 +0900

    tailf.js: 開くファイルの文字エンコーディングがANSIかUTF-16かを判定して開くようにした。

--- tailf.js
+++ tailf.js
@@ -3,11 +3,43 @@ function main() {
 	if (args.Length < 1) {
 		return;
 	}
-	var fso = new ActiveXObject("Scripting.FileSystemObject");
-	var filePath = args(0);
-	var ForReading = 1;
-	var f = fso.OpenTextFile(filePath, ForReading);
-	var stopCounter = 0;
+    var filePath = args(0);
+    var isUTF16 = false;
+    var isUTF8 = false;
+
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    var fileSize = fso.GetFile(filePath).Size;
+
+    // UTF-8 or UTF-16 or ANSI
+    var stream = new ActiveXObject("ADODB.Stream");
+    var adTypeText = 2;
+    stream.Type = adTypeText;
+    stream.Charset = 'iso-8859-1';
+    stream.Open();
+    stream.LoadFromFile(filePath);
+    stream.Position = 0;
+    if (fileSize >= 2) {
+        var head = stream.ReadText(2);
+        if (head === "\xFF\xFE") {
+            isUTF16 = true;
+        } else if (head === "\xEF\xBB") {
+            if (fileSize >= 3) {
+                var head2 = stream.ReadText(1);
+                if (head2 === "\xBF") {
+                    isUTF8 = true;
+                }
+            }
+        }
+    }
+    stream.Close();
+
+    var ForReading = 1;
+    var TristateUseDefault = -2;
+    var TristateUseTrue = -1;
+    var TristateUseFalse = 0;
+    var format = isUTF16 ? TristateUseTrue : TristateUseFalse;
+    var f = fso.OpenTextFile(filePath, ForReading, false, format);
+    var stopCounter = 0;
 	while (true) {
 		if (f.AtEndOfStream) {
 			stopCounter++;
